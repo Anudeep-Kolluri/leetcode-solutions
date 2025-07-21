030c8288ec28c4819bcf2c4f66a0227b619818bc
Anudeep Kolluri <50168940+Anudeep-Kolluri@users.noreply.github.com>
2025-07-21 00:59:37 -0500

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.py
@@ -0,0 +1,13 @@
+# Last updated: 7/21/2025, 12:59:37 AM
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+        while num:
+            if num % 2 == 0:
+                num = num // 2
+            else:
+                num -= 1
+            
+            steps += 1
+
+        return steps
