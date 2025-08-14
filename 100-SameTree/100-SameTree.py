c271d1c5b70cce7fdfe2ed0570997974a5a44dfa
Anudeep Kolluri <50168940+Anudeep-Kolluri@users.noreply.github.com>
2025-08-14 08:35:37 -0700

    File updated at 8/14/2025, 8:35:37 AM

--- 100-SameTree/100-SameTree.py
+++ 100-SameTree/100-SameTree.py
@@ -1,4 +1,4 @@
-# Last updated: 7/21/2025, 1:01:05 AM
+# Last updated: 8/14/2025, 8:35:37 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,11 +7,42 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None or q  == None:
-            return True if p == q else False
+        # MUST USE BFS
 
-        if p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        if p == q:
+            return True
 
-        else:
-            return False
+        queue1 = [p]
+        queue2 = [q]
+
+        visited1 = []
+        visited2 = []
+
+        while queue1 and queue2:
+
+            curr_1 = queue1.pop(0)
+            curr_2 = queue2.pop(0)
+
+            first = (curr_1 == None)
+            second = (curr_2 == None)
+
+            if first or second:
+                if first != second:
+                    return False
+                else:
+                    continue
+
+            elif curr_1.val != curr_2.val:
+                return False
+
+            
+            if curr_1.left != None or curr_1.right != None:
+                queue1.append(curr_1.left)
+                queue1.append(curr_1.right)
+            
+            if curr_2.left != None or curr_2.right != None:
+                queue2.append(curr_2.left)
+                queue2.append(curr_2.right)
+
+        
+        return True if (len(queue1) == 0) and (len(queue2) == 0) else False
