eeb4ea5363ac41a44f4dbe65b0016bb1cf7f7819
Anudeep Kolluri <50168940+Anudeep-Kolluri@users.noreply.github.com>
2025-08-16 10:39:08 -0700

    Create 133-CloneGraph

---
+++ 133-CloneGraph/version/python/133-CloneGraph_20250816_103908.py
@@ -0,0 +1,18 @@
+# Last updated: 8/16/2025, 10:39:08 AM
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return node
+        
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft() 
+            cur_clone = clones[cur.val]            
+
+            for ngbr in cur.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+                    
+                cur_clone.neighbors.append(clones[ngbr.val])
+                
+        return clones[node.val]
